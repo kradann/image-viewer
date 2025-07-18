869ea444b39d0f89c9a94c3dd16642895e20e040
krezsnely <krasznai.daniel1219@gmail.com>
2025-07-18 16:36:49 +0200

    loading method change, not sure, it's faster

--- montage_batch.py
+++ montage_batch.py
@@ -3,9 +3,12 @@ import io
 import json
 import os
 import shutil
+import hashlib
+import time
 from time import sleep
 from typing import Union
 
+import PIL.Image
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PIL import Image
 from PyQt5.QtCore import QTimer, Qt
@@ -30,20 +33,40 @@ def pil_to_qpixmap(pil_image):
     qimg = QImage.fromData(buffer.getvalue(), "PNG")
     return QPixmap.fromImage(qimg)
 
+def get_thumb_path(image_path: str, cache_dir=".thumbs") -> str:
+    os.makedirs(cache_dir, exist_ok=True)
+    hash_name = hashlib.md5(image_path.encode("utf-8")).hexdigest()
+    return os.path.join(cache_dir, f"{hash_name}.jpg")
+
+def generate_thumbnail(image_path: str, thumb_path: str, size=(800, 800)):
+    try:
+        with Image.open(image_path) as img:
+            img = img.convert("RGB")
+            img.thumbnail(size, Image.LANCZOS)
+            img.save(thumb_path, "JPEG")
+    except Exception as e:
+        print(f"Thumbnail error for {image_path}: {e}")
 
 class ImageLoaderThread(QtCore.QThread):
     image_loaded = QtCore.pyqtSignal(int, QtGui.QPixmap, str)
 
-    def __init__(self, paths):
+    def __init__(self, paths, cache_dir=".thumbs"):
         super().__init__()
         self.paths = paths
+        self.cache_dir = cache_dir
 
     def run(self):
         for idx, path in enumerate(self.paths):
+            print(f"[LoaderThread] Loading: {path}")
+            thumb_path = get_thumb_path(path, cache_dir=self.cache_dir)
+
+            if not os.path.exists(thumb_path):
+                generate_thumbnail(path, thumb_path)
+
             try:
-                img = Image.open(path)
+                #img = Image.open(path)
                 # img.thumbnail((128, 128))
-                pixmap = pil_to_qpixmap(img)
+                pixmap = QtGui.QPixmap(thumb_path)
                 # qimage = ImageQt(img)
                 # pixmap = QtGui.QPixmap.fromImage(qimage)
                 self.image_loaded.emit(idx, pixmap, path)
@@ -391,6 +414,7 @@ class ImageMontageApp(QtWidgets.QWidget):
             QMenuBar {
                 background-color: #181a1b;
                 font-size: 18px;
+                
             }
 
             QMenuBar::item {
